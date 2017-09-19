1e0b07cab7f9c91e3cb25518c0fb560acaa85fcf
wwhurin <wwhurin@e-mirim.hs.kr>
2017-09-19 20:51:23 +0900

    #5 웹 브라우저로 깃 띄우기

--- app/src/main/java/kr/hs/emirim/wwhurin/firebasedbexample/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/wwhurin/firebasedbexample/MainActivity.java
@@ -1,13 +1,16 @@
 package kr.hs.emirim.wwhurin.firebasedbexample;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView mNameTextView;
     private TextView mGitHubTextView;
@@ -27,5 +30,19 @@ public class MainActivity extends AppCompatActivity {
                 .centerCrop()
                 .resize(100, 100)
                 .into(mProfilImageView);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.profile_github:
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Uri uri = Uri.parse(mGitHubTextView.getText().toString() );
+                intent.setData(uri);
+                startActivity(intent);
+
+                break;
+            default:
+        }
     }
 }
