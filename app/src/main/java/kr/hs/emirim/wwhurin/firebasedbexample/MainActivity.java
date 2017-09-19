4eec3fec4911286e5dd2f71c5d320454dcb230e4
wwhurin <wwhurin@e-mirim.hs.kr>
2017-09-19 20:27:11 +0900

    #4 피카소와의 만남

--- app/src/main/java/kr/hs/emirim/wwhurin/firebasedbexample/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/wwhurin/firebasedbexample/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mNameTextView;
@@ -21,5 +23,9 @@ public class MainActivity extends AppCompatActivity {
         mGitHubTextView = (TextView)findViewById(R.id.profile_github);
         mProfilImageView = (ImageView) findViewById(R.id.profile_image);
 
+        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png")
+                .centerCrop()
+                .resize(100, 100)
+                .into(mProfilImageView);
     }
 }
